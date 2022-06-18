13b2cc499eb66a76e7382af6d43f2ff4a2c28d38
Nick Warren <nickwarren47@gmail.com>
2022-06-18 12:36:20 -0600

    Added affiliation links to bottom of website.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 // const h2 = document.createElement("h2");
 // h2.textContent = "This content added by Javascript";
 // document.querySelector("body").appendChild(h2);
+function webPageBorder() {
+    document.getElementById("whole").style.border = "thick solid #0000FF";
+  }
